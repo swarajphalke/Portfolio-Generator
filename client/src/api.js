188a219e2a478febda60d948e599905a01497199
Swaraj Phalke <swarajphalke555@gmail.com>
2025-08-29 21:25:26 +0530

    updated api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,9 @@
 
 import axios from 'axios'
 
-export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:4000'
+export const API_BASE =
+  import.meta.env.VITE_API_BASE ||
+  "https://portfolio-generator-huy5.onrender.com";
 const api = axios.create({ baseURL: API_BASE })
 
 export const getPortfolios = async (params={}) => {
